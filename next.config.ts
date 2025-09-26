6966a2ae0e7556589e045ada0834bc78ebf7f026
quehill <quehill@gmail.com>
2025-09-26 10:18:00 -0400

    update basepath

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
       images: {
         unoptimized: true, // Optional: if you use Image component, set unoptimized to true for static export
       },
-      basePath: process.env.NODE_ENV === 'production' ? '/oss_catalog/' : '', // Important for correct pathing on GitHub Pages
-      assetPrefix: process.env.NODE_ENV === 'production' ? '/oss_catalog/' : '', // Important for correct pathing on GitHub Pages
+      basePath: process.env.NODE_ENV === 'production' ? '/oss_catalog' : '', // Important for correct pathing on GitHub Pages
+      assetPrefix: process.env.NODE_ENV === 'production' ? '/oss_catalog' : '', // Important for correct pathing on GitHub Pages
 };
 
 export default nextConfig;
