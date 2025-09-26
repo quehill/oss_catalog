15cddfe5514b1c3b03aff6d8abe2c42ce51016e9
quehill <quehill@gmail.com>
2025-09-26 10:00:07 -0400

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+      images: {
+        unoptimized: true, // Optional: if you use Image component, set unoptimized to true for static export
+      },
+      basePath: process.env.NODE_ENV === 'production' ? '/oss_catalog/' : '', // Important for correct pathing on GitHub Pages
+      assetPrefix: process.env.NODE_ENV === 'production' ? '/oss_catalog/' : '', // Important for correct pathing on GitHub Pages
 };
 
 export default nextConfig;
